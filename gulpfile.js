722a0228431618079f18d8d40b05560301dde212
Andrey <aa@stepcart.com>
2017-11-27 03:02:04 +0300

    Base structure

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const path = {
 gulp.task('default', ['pug', 'sass', 'assets']);
 
 gulp.task('pug', function () {
-    return gulp.src('src/**/*.pug')
+    return gulp.src('src/index.pug')
         .pipe(pug({ pretty: true }))
         .pipe(gulp.dest('build'));
 });
